95deeba9ef9d3de661507715ba3232a314a748c2
EZRCRZ <93253858+EZRCRZ@users.noreply.github.com>
2025-03-24 12:59:40 +0800

    revert appservice and app.blade

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -4,30 +4,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <!-- CSRF Token -->
+    <!-- csrf token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ ucfirst(AppSettings::get('app_name', 'App')) }} - {{ ucfirst($title ?? '') }}</title>
-    
+    <title>{{ucfirst(AppSettings::get('app_name', 'App'))}} - {{ucfirst($title ?? '')}}</title>
     <!-- Favicon -->
-    <link rel="shortcut icon" type="image/x-icon" 
-          href="{{ !empty(AppSettings::get('favicon')) ? secure_asset('storage/' . AppSettings::get('favicon')) : secure_asset('assets/img/favicon.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{!empty(AppSettings::get('favicon')) ? asset('storage/'.AppSettings::get('favicon')) : asset('assets/img/favicon.png')}}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/fontawesome/css/fontawesome.min.css')}}">
+    <!-- Feathericon CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/feathericon.min.css')}}">
 
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/plugins/fontawesome/css/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/feathericon.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/icons.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/plugins/snackbar/snackbar.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/plugins/sweetalert2/sweetalert2.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/plugins/select2/css/select2.min.css') }}">
-    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
-    
-    <!-- Page Specific CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/icons.min.css')}}">
+    <!-- Snackbar CSS -->
+	<link rel="stylesheet" href="{{asset('assets/plugins/snackbar/snackbar.min.css')}}">
+    <!-- Sweet Alert css -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/sweetalert2/sweetalert2.min.css')}}">
+    <!-- Snackbar Css -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/snackbar/snackbar.min.css')}}">
+    <!-- Select2 Css -->
+    <link rel="stylesheet" href="{{asset('assets/plugins/select2/css/select2.min.css')}}">
+    <!-- Main CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+    <!-- Page CSS -->
     @stack('page-css')
-
     <!--[if lt IE 9]>
-        <script src="{{ secure_asset('assets/js/html5shiv.min.js') }}"></script>
-        <script src="{{ secure_asset('assets/js/respond.min.js') }}"></script>
+        <script src="assets/js/html5shiv.min.js"></script>
+        <script src="assets/js/respond.min.js"></script>
     <![endif]-->
 </head>
 <body>
@@ -37,13 +41,17 @@
 
         <!-- Header -->
         @include('admin.includes.header')
+        <!-- /Header -->
 
         <!-- Sidebar -->
         @include('admin.includes.sidebar')
+        <!-- /Sidebar -->
 
         <!-- Page Wrapper -->
         <div class="page-wrapper">
+
             <div class="content container-fluid">
+
                 <!-- Page Header -->
                 <div class="page-header">
                     <div class="row">
@@ -51,7 +59,6 @@
                     </div>
                 </div>
                 <!-- /Page Header -->
-
                 @if ($errors->any())
                     @foreach ($errors->all() as $error)
                         <x-alerts.danger :error="$error" />
@@ -59,10 +66,9 @@
                 @endif
 
                 @yield('content')
-
-                <!-- Add Sales Modal -->
+                <!-- add sales modal-->
                 <x-modals.add-sale />
-                <!-- /Add Sales Modal -->
+                 <!-- / add sales modal -->
             </div>
         </div>
         <!-- /Page Wrapper -->
@@ -71,17 +77,21 @@
     <!-- /Main Wrapper -->
     
 </body>
+<!-- jQuery -->
+<script src="{{asset('assets/plugins/jquery/jquery.min.js')}}"></script>
 
-<!-- Scripts -->
-<script src="{{ secure_asset('assets/plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ secure_asset('assets/js/popper.min.js') }}"></script>
-<script src="{{ secure_asset('assets/js/bootstrap.min.js') }}"></script>
-<script src="{{ secure_asset('assets/plugins/slimscroll/jquery.slimscroll.min.js') }}"></script>
-<script src="{{ secure_asset('assets/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-<script src="{{ secure_asset('assets/plugins/snackbar/snackbar.min.js') }}"></script>
-<script src="{{ secure_asset('assets/plugins/select2/js/select2.min.js') }}"></script>
-<script src="{{ secure_asset('assets/js/script.js') }}"></script>
-
+<!-- Bootstrap Core JS -->
+<script src="{{asset('assets/js/popper.min.js')}}"></script>
+<script src="{{asset('assets/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('assets/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
+<!-- Sweet Alert Js -->
+<script src="{{asset('assets/plugins/sweetalert2/sweetalert2.min.js')}}"></script>
+<!-- Snackbar Js -->
+<script src="{{asset('assets/plugins/snackbar/snackbar.min.js')}}"></script>
+<!-- Select2 JS -->
+<script src="{{asset('assets/plugins/select2/js/select2.min.js')}}"></script>
+<!-- Custom JS -->
+<script src="{{asset('assets/js/script.js')}}"></script>
 <script>
     $(document).ready(function(){
         $('body').on('click','#deletebtn',function(){
@@ -92,12 +102,12 @@
                     title: "Are you sure?",
                     text: "You won't be able to revert this!",
                     type: "warning",
-                    showCancelButton: true,
+                    showCancelButton: !0,
                     confirmButtonText: '<i class="fe fe-trash mr-1"></i> Delete!',
                     cancelButtonText: '<i class="fa fa-times mr-1"></i> Cancel!',
                     confirmButtonClass: "btn btn-success mt-2",
                     cancelButtonClass: "btn btn-danger ml-2 mt-2",
-                    buttonsStyling: false,
+                    buttonsStyling: !1,
                     preConfirm: function(){
                         return new Promise(function(){
                             $.ajax({
@@ -110,7 +120,7 @@
                                             title: "Deleted!",
                                             text: "Resource has been deleted.",
                                             type: "success",
-                                            showConfirmButton: false,
+                                            showConfirmButton: !1,
                                             timer: 1500,
                                         })
                                     )
@@ -124,7 +134,6 @@
             ]).catch(swal.noop);
         }); 
     });
-
     @if(Session::has('message'))
         var type = "{{ Session::get('alert-type', 'info') }}";
         switch(type){
@@ -135,6 +144,7 @@
                     backgroundColor: '#2196f3'
                 });
                 break;
+
             case 'warning':
                 Snackbar.show({
                     text: "{{ Session::get('message') }}",
@@ -143,6 +153,7 @@
                     backgroundColor: '#e2a03f'
                 });
                 break;
+
             case 'success':
                 Snackbar.show({
                     text: "{{ Session::get('message') }}",
@@ -151,6 +162,7 @@
                     backgroundColor: '#8dbf42'
                 });
                 break;
+
             case 'danger':
                 Snackbar.show({
                     text: "{{ Session::get('message') }}",
@@ -162,8 +174,6 @@
         }
     @endif
 </script>
-
-<!-- Page Specific Scripts -->
+<!-- Page JS -->
 @stack('page-js')
-
 </html>
